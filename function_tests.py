5bd4cdffdde2f878e9d3508db0089237022c6bf5
unknown <c_luyuan-001@SU.INTRA.CPIC.COM.CN>
2018-12-13 14:20:22 +0800

    Unittest

---
+++ function_tests.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+
+class NewVistorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser=webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+        # 断言 assertIn assertEqual assertTrue assertFalse
+        self.assertIn('To-do',self.browser.title)
+
+        self.fail("Finish the test")
+
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
+
+
