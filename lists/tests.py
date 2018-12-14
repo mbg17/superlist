5bf3925d03cc231fdd499fcdd61663789e82acb1
unknown <c_luyuan-001@SU.INTRA.CPIC.COM.CN>
2018-12-14 15:54:14 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from lists.views import home_page
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 # Create your tests here.
 
@@ -34,9 +35,31 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-     # 测试能否成功响应POST请求
+    # 测试能否成功响应POST请求
     def test_can_save_a_POST_request(self):
         #向目标地址传送数据 data={标签name:值}
-        response=self.client.post('/',data={'item_text':'A new list item'})
-        self.assertIn('A new list item',response.content.decode())
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+    # 数据模型测试
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        # 存储数据
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        # 获取所有数据对象
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
