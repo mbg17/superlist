4978916770fa5dd98033d1cc0755ee12c873604d
unknown <c_luyuan-001@SU.intra.cpic.com.cn>
2018-12-14 10:17:53 +0800

    Basic viwe now returns minimal HTML

--- function_tests.py
+++ function_tests.py
@@ -11,7 +11,7 @@ class NewVistorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
         # 断言 assertIn assertEqual assertTrue assertFalse
-        self.assertIn('To-do',self.browser.title)
+        self.assertIn('To-Do',self.browser.title)
 
         self.fail("Finish the test")
 
