7cc9ab2c737ed4d0f9378b6c77884a44f9054c41
unknown <c_luyuan-001@SU.INTRA.CPIC.COM.CN>
2018-12-20 17:09:49 +0800

    post 报错

--- lists/urls.py
+++ lists/urls.py
@@ -13,17 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from lists import views as list_views
-from django.conf.urls import url,include
-from lists import urls as list_urls
+# from django.contrib import admin
+# from django.urls import path
+from lists import views
+from django.conf.urls import url
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^$',list_views.home_page,name='home'),
-    url(r'^lists/',include(list_urls)),
+    # path('admin/', admin.site.urls),
+    # url(r'^$',views.home_page,name='home_page'),
     # 捕获组(capture group,.+) 匹配/后面的任意字符
-    # url(r'^lists/(\d+)/$',views.view_list,name='view_list'),
-    # url(r'^lists/new$',views.new_list,name='new_list'),
-    # url(r'^lists/(\d+)/add_item$',views.add_item,name='add_item'),
+    url(r'^(\d+)/$',views.view_list,name='view_list'),
+    url(r'^new$',views.new_list,name='new_list'),
+    url(r'^(\d+)/add_item$',views.add_item,name='add_item'),
 ]
