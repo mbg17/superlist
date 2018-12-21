e8c1ff19d72a4cb97b0ed4f321ceb8a10bb8e79b
unknown <c_luyuan-001@SU.INTRA.CPIC.COM.CN>
2018-12-21 10:48:51 +0800

    new url,foreignkey complete

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from lists import views as list_views
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from lists import urls as list_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$',list_views.home_page,name='home'),
-    url(r'^lists/',include(list_urls)),
+    url(r'^$', list_views.home_page, name='home'),
+    # 去重，前缀为^list/，后缀通过项目文件下的urls匹配url，拓展性强
+    url(r'^lists/', include(list_urls)),
     # 捕获组(capture group,.+) 匹配/后面的任意字符
     # url(r'^lists/(\d+)/$',views.view_list,name='view_list'),
     # url(r'^lists/new$',views.new_list,name='new_list'),
